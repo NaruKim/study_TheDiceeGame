7d83fff13b699e4494f2c0599ffab6b411d9088d
WolfPilgrim <58687191+WolfPilgrim@users.noreply.github.com>
2020-01-03 21:10:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,69 @@
-var n=Math.random()*6;
-var m=Math.floor(n);
+//First Die
+var n = Math.random();
+var m = Math.floor(n * 6);
+//Second Die
+var x = Math.random();
+var y = Math.floor(x * 6);
 
-var x=Math.random()*6;
-var y=Math.floor(x);
+if(m>y){
+  document.querySelector("h2").textContent="Player 1 Won";
+}
+else if(y>m){
+  document.querySelector("h2").textContent="Player 2 Won";
+}
+else if(m==y){
+  document.querySelector("h2").textContent="Draw";
+}
 
-document.getElementsByClassName("img1").innerHTML = "src="dic1.png">";
+//First Die
+
+if (m == 0) {
+  document.querySelector(".img1").setAttribute("src", "dice1.png");
+}
+
+if (m == 1) {
+  document.querySelector(".img1").setAttribute("src", "dice2.png");
+}
+
+if (m == 2) {
+  document.querySelector(".img1").setAttribute("src", "dice3.png");
+}
+
+if (m == 3) {
+  document.querySelector(".img1").setAttribute("src", "dice4.png");
+}
+
+if (m == 4) {
+  document.querySelector(".img1").setAttribute("src", "dice5.png");
+}
+
+if (m == 5) {
+  document.querySelector(".img1").setAttribute("src", "dice6.png");
+}
+
+//Second Die
+
+
+if (y == 0) {
+  document.querySelector(".img2").setAttribute("src", "dice1.png");
+}
+
+if (y == 1) {
+  document.querySelector(".img2").setAttribute("src", "dice2.png");
+}
+
+if (y == 2) {
+  document.querySelector(".img2").setAttribute("src", "dice3.png");
+}
+
+if (y == 3) {
+  document.querySelector(".img2").setAttribute("src", "dice4.png");
+}
+
+if (y == 4) {
+  document.querySelector(".img2").setAttribute("src", "dice5.png");
+}
+
+if (y == 5) {
+  document.querySelector(".img2").setAttribute("src", "dice6.png");
+}
